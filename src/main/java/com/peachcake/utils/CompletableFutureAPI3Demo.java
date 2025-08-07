f94899ce40bb10ebe608cb7e0c116f39ef4d1853
lmzhangzhongtao <80764323+lmzhangzhongtao@users.noreply.github.com>
2025-08-07 11:50:32 +0800

    JUC代码

--- src/main/java/com/peachcake/utils/CompletableFutureAPI3Demo.java
+++ src/main/java/com/peachcake/utils/CompletableFutureAPI3Demo.java
@@ -4,16 +4,20 @@ import java.util.concurrent.CompletableFuture;
 
 public class CompletableFutureAPI3Demo {
     public static void main(String[] args) {
-        CompletableFuture.supplyAsync(() -> {
-            return 1;
-        }).thenApply(f -> {
-            return f + 2;
-        }).thenApply(f -> {
-            return f + 3;
-        }).thenAccept(System.out::println);
-//                .thenAccept(r ->{
-//            System.out.println(r);
-//        });
+//        CompletableFuture.supplyAsync(() -> {
+//            return 1;
+//        }).thenApply(f -> {
+//            return f + 2;
+//        }).thenApply(f -> {
+//            return f + 3;
+//        }).thenAccept(System.out::println);
+////                .thenAccept(r ->{
+////            System.out.println(r);
+////        });
+        System.out.println(CompletableFuture.supplyAsync(() -> "resultA").thenRun(() -> {
+        }).join());
+        System.out.println(CompletableFuture.supplyAsync(() -> "resultA").thenAccept(r -> System.out.println(r)).join());
+//       System.out.println(CompletableFuture.supplyAsync(() -> "resultA").thenApply(r -> r + "resultB").join());
 
     }
 }
